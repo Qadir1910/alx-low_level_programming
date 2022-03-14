21132b4837aae5fc1cfb30a2007192fa7762e799
root <q.adio1910@gmail.com                     git config --global user.name qadir1910>
2022-03-14 10:28:11 -0700

    push it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ * main - Enty point
+ *
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
