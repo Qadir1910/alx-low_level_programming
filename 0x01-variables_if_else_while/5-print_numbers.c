7a6d97e98b1bda62351277c70e21d2cbc3559bfa
root <q.adio1910@gmail.com                     git config --global user.name qadir1910>
2022-03-15 07:19:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits base 10
+ * Return: zero
+ */
+
+int main(void)
+{
+	int rza;
+
+	for (rza = 0; rza < 10; rza++)
+		printf("%d", rza);
+
+	printf("\n");
+
+	return (0);
+}
