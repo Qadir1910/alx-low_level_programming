d88fe4dd74c7a08f93e2e773aa89f32d8fb3ebac
root <q.adio1910@gmail.com                     git config --global user.name qadir1910>
2022-03-17 07:04:55 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
- *print_last_digit - print the last digit
- *@n: function
- *Return: valve 0
+ *print_last_digit - last digit of number
+ *@n: input.
+ *
+ *Return: last digit of number.
  */
 int print_last_digit(int n)
 {
-	int j;
-	j = (n % 10);
-	if (j < 0)
+	int lastDig;
+	if (n >= 0)
 	{
-		j = j * (-1);
+		lastDig = n % 10;
 	}
-	_putchar(j + 48);
-	return (j);
+	else
+
+	{
+
+		lastDig = (n % 10) * -1;
+
+	}
+
+	_putchar('0' + lastDig);
+
+	return (lastDig);
 }
