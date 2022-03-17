887bf86c7512f067d293d6e41a33231ba43bcae8
root <q.adio1910@gmail.com                     git config --global user.name qadir1910>
2022-03-17 03:33:09 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- *print_alphabet - abcd
- *Description: print lowercase alphabet
- *Return: void.
+ * print_alphabet - A function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-	for (i = 0; i < 26; i++)
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		_putchar(alphabet[i]);
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
